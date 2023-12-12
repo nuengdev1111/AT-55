ebf616d769d1b248a23bd4dec2be7975b503750e
earth <earthlovely60000@gmail.com>
2023-12-13 00:24:16 +0700

    update page home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,14 @@
 import Background from "@/components/background";
 import { Container } from "@mui/material";
-import Header from "@/components/header";
-import Breadcrumbs from "../components/breadcrumb";
-import btnintro from "../../public/btnintro.png"
-import Image from "next/image";
-const menu = [
-  { name: "", rounte: "", status: "active"},
-];
+
+
 
 export default function Home() {
   return (
     <main>
-      <Container maxWidth="lg">
-        <Header />
-      </Container>
-      <Breadcrumbs menu={menu} />
-      <Container maxWidth="lg">
+      
+      <Container>
         <Background />
-        <div className="box-btn" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <a href="/home">
-          <Image
-          src="/btnintro.png"
-          alt="btn-intro"
-          width={200}
-          height={200}
-          
-          />
-          </a>
-        </div>
       </Container>
     </main>
   );
--- src/components/background/index.jsx
+++ src/components/background/index.jsx
@@ -1,21 +1,44 @@
-"use client"
-import Image from 'next/image'
-import King from '../../../public/king.png'
-import styled from './background.module.scss'
- 
+import Image from 'next/image';
+import styled from './background.module.scss';
+
 export default function Background() {
   return (
-    
+    <div className={styled.background}>
+      <div className={styled.backgroundImage}>
+        <Image src="/bg.jpg" alt="Background Image" layout="fill" />
+      </div>
+      <a href='/home' className={styled.overlayImage}>
+        <Image src="/btnintro.png" alt="Overlay Image" height={1000} width={350} />
+      </a>
+      <div className={styled.overlayImage2}>
+        <Image src="/5dec.png" alt="Overlay Image2" height={1000} width={400} />
+      </div>
+      <div className={styled.overlayImage3}>
+        <Image src="/king1.png" alt="Overlay Image3" height={500} width={500} />
+      </div>
+     <div className={styled.textoverlay}>
+      <ol className={styled.overlaytext}>
+  <li >
+           เนื่องในโอกาสวันคล้ายวันพระบรมราชสมภพ
+  </li>
+  <li>
+  พระบาทสมเด็จพระบรมชนกาธิเบศร มหาภูมิพลอดุลยเดชมหาราช บรมนาถบพิตร
+  </li>
+  <li>
+  วันชาติ และวันพ่อแห่งชาติ
+  </li>
+  <li>
+  น้อมรำลึกในพระมหากรุณาธิคุณเป็นล้นพ้นอย่างหาที่สุดมิได้
+  </li>
+  <li>
+  ข้าพระพุทธเจ้า ผู้บริหาร ข้าราชการ พนักงานและลูกจ้าง
+  </li>
+  <li>
+  องค์การบริหารส่วนตำบลปกาสัย อ.เสาธง จ.สมุทรปราการ  (อบต.บางเสาธง)
+  </li>
+</ol>
+</div>
 
-    <Image
-      className={styled.img}
-      src={King}
-      width={0}
-      height={0}
-      alt='king'
-    />
-  
-      
-    
-  )
+    </div>
+  );
 }
