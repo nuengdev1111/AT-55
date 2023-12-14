ff2a45ad2d32e3a7a217919a3188ffa6d114d914
jiffy <jiffany.napaporn@gmail.com>
2023-12-14 11:04:51 +0700

    edit component editprofile historyrequeststatus

--- src/app/historyRequestStatus/page.jsx
+++ src/app/historyRequestStatus/page.jsx
@@ -5,49 +5,16 @@ import Header from "../../components/header";
 import Breadcrumb from "../../components/breadcrumb";
 import Image from 'next/image';
 import styles from "./hitstoryRequestStatus.module.scss"
-
+import HitstoryRequestStatus from "../../components/history";
 
 const menu = [
   { name: "ประวัติการยื่นคำร้อง", route: "", status: "active" },
 ];
 
-const HistoryRequestTable = () => {
-  const data = [
-    { request: 'คำร้องที่ 1', file: 'file1.pdf', date: '2023-01-01', time: '10:00', status: 'รับเรื่องแล้ว' },
-    { request: 'คำร้องที่ 2', file: 'file2.pdf', date: '2023-01-02', time: '14:30', status: 'รับเรื่องแล้ว' },
-  ];
 
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>คำร้อง</TableCell>
-          <TableCell>ไฟล์คำร้อง</TableCell>
-          <TableCell>วันที่</TableCell>
-          <TableCell>เวลา</TableCell>
-          <TableCell>สถานะ</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((row, index) => (
-          <TableRow key={index}>
-            <TableCell>{row.request}</TableCell>
-            <TableCell>
-              <Link href={`/path/to/pdf/${row.file}`} target="_blank" rel="noopener noreferrer">
-                {row.file}
-              </Link>
-            </TableCell>
-            <TableCell>{row.date}</TableCell>
-            <TableCell>{row.time}</TableCell>
-            <TableCell>{row.status}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
-};
 
-export default function HitstoryRequestStatus() {
+
+export default function Hitstory() {
   return (
     <main className="">
       <Container maxWidth="lg">
@@ -64,18 +31,9 @@ export default function HitstoryRequestStatus() {
     />
 
       <Breadcrumb menu={menu} />
-
-      <Box className="mt-2 flex justify-center" sx={{ marginTop: '3rem', marginBottom: '5rem' }}>
-        <Card variant="outlined" sx={{ maxWidth: 1000, width: "100%", backgroundColor: '#f0f0f0', border: 'none' }}>
-          <Box sx={{ p: { xs: 2, md: 5 } }}>
-            <Typography className="text-center pb-10" variant="h5">
-              <strong>ประวัติการยื่นคำร้อง</strong>
-            </Typography>
-
-            <HistoryRequestTable />
-          </Box>
-        </Card>
-      </Box>
-    </main>
+      <Container maxWidth="lg">
+          <HitstoryRequestStatus/>
+      </Container>
+            </main>
   );
 }
