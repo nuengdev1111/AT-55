7ebc9d6a4f3d4c34277939103396a9dff265ba9e
earth <earthlovely60000@gmail.com>
2023-12-15 19:03:48 +0700

    update logo and banner responsive

--- src/app/executiveboard/doSecretary/page.jsx
+++ src/app/executiveboard/doSecretary/page.jsx
@@ -24,7 +24,7 @@ export default function executive() {
       <DoSecretary/>
      
       </Container>
-      <Footer/>
+      {/* <Footer/> */}
     </main>
   );
 }
--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -2,40 +2,56 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
-import { images } from "../../../lib/images";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import styled from "./banner.module.scss";
 
+const images = [
+  "/first.png",
+  "/second.jpg",
+  "/third.jpg",
+  // เพิ่มรูปภาพอื่น ๆ ตามต้องการ
+];
+
 export default function Page() {
   return (
     <section>
       <div>
-      <Swiper
-  navigation
-  pagination={{ type: "fraction" }}
-  modules={[Navigation, Pagination]}
-  onSwiper={(swiper) => console.log(swiper)}
->
-  {images.map((image, index) => (
-    <SwiperSlide key={index}>
-      <div className={styled.swiperimagecontainer}>
-        <Image
-          src={image.src}
-          alt={image.alt}
-          layout="fill"
-          objectFit="cover"
-          
-        />
+
+        <Swiper
+          autoHeight={true}
+          spaceBetween={20}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation, Pagination]}
+          className="mySwiper"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={image}
+                alt={`Slide ${index}`}
+                width={1000}
+                height={400}
+                layout="responsive"
+              />
+              <div className={styled.overlay}>
+          <div className={styled.logo}>
+            <img src="/logo.png" alt="Logo" />
+          </div>
+          {/* <div className={styled.text}>
+          <h1 className={styled.headingStyle}>องค์การบริหารส่วนตำบลบางเสาธง</h1>
+            <h2 className={styled.headingStyle2}>Bangsaotong Subdistrict Administrative Organization</h2>
+          </div> */}
+        </div>
+      
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-    </SwiperSlide>
-  ))}
-</Swiper>
-
-</div>
-
-
     </section>
   );
 }
